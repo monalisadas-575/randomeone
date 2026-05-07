f8e48b2b111ed800934c6079d9e3e198ea353229
Monalisa Das <monalisadas575@gmail.com>
2026-05-07 10:49:59 +0530

    DeadLock and its safe unlock

--- src/engineeringDigest/multithraedingPractice/myThreadPool/MultiThreadMain.java
+++ src/engineeringDigest/multithraedingPractice/myThreadPool/MultiThreadMain.java
@@ -1,22 +1,25 @@
 package engineeringDigest.multithraedingPractice.myThreadPool;
 
 public class MultiThreadMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         long startTime = System.currentTimeMillis();
         Thread[] threads  = new Thread[9];
         for(int i = 1; i<10; i++){
             int finalI = i;
             threads[i-1] = new Thread(() -> {
                 long result = factorial(finalI);
-                System.out.println(result);
+                System.out.println(Thread.currentThread().getName() +result);
             });
             threads[i-1].start();
+        }
+        for(Thread t : threads){
+            t.join();
         }
         System.out.println("Total time "+ (System.currentTimeMillis()- startTime));
 
     }
 
-    private static long factorial(int n){
+    private  static long factorial(int n){
         try{
             Thread.sleep(1000);
         }catch(InterruptedException e){
