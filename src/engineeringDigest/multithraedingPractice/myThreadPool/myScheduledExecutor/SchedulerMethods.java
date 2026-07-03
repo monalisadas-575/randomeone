8288ee6879ea7837ff18f79a0bc75e3941b0d1ed
Monalisa Das <monalisadas575@gmail.com>
2026-07-03 20:58:20 +0530

    minor changes in Stream

--- src/engineeringDigest/multithraedingPractice/myThreadPool/myScheduledExecutor/SchedulerMethods.java
+++ src/engineeringDigest/multithraedingPractice/myThreadPool/myScheduledExecutor/SchedulerMethods.java
@@ -9,7 +9,14 @@ public class SchedulerMethods {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(()->{    //it will print in every 5 second
             System.out.println("task executed after every 5 second");
-        },5,5, TimeUnit.SECONDS);
+        },5,5, TimeUnit.SECONDS);  //here with scheduleAtFixedRate() after every 5 sec will run the task.
+        // if the task require 10 sec to finish still it will run at every 5 sec.
+
+        scheduler.scheduleWithFixedDelay(()->{
+            System.out.println("task executed after every 5 second");
+        },5,5,TimeUnit.SECONDS);//here with scheduleWithFixedDelay()
+        // it will run the task after 5sec of finishing the task.
+        // if the task require 10 sec to finish still it will run at every 5 sec.
 
         scheduler.schedule(()->{    // it will shutdown after the timeout.
             System.out.println("Intiating shutDown");
